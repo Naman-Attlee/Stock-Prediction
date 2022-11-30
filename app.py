610cc00a2224ca4448af004c62dc36ca46951c7a
Naman-Attlee <111482369+Naman-Attlee@users.noreply.github.com>
2022-11-30 16:49:50 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,81 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from pandas_datareader import data
+from keras.models import load_model
+import streamlit as st
+
+
+start = '2010-01-01'
+end = '2021-12-31'
+
+st.title('Stock Prediction')
+user_input = st.text_input('Enter Stock Ticker', 'AAPL')
+df = data.DataReader(user_input,'yahoo',start,end)
+
+
+# describing data
+
+st.subheader('Data from 2010 - 2021')
+st.write(df.describe())
+
+# visualization
+
+st.subheader('Closing Price vs Time chart with 100ma')
+ma100 = df.Close.rolling(100).mean()
+fig = plt.figure(figsize=(12,6))
+plt.plot(ma100)
+plt.plot(df.Close)
+st.pyplot(fig)
+
+st.subheader('Closing Price vs Time chart with 100ma and 200ma')
+ma100 = df.Close.rolling(100).mean()
+ma200 = df.Close.rolling(200).mean()
+fig = plt.figure(figsize=(12,6))
+plt.plot(ma100,'r')
+plt.plot(ma200,'g')
+plt.plot(df.Close,'b')
+st.pyplot(fig)
+
+
+data_traning = pd.DataFrame(df['Close'][0:int(len(df)*0.70)])
+data_testing = pd.DataFrame(df['Close'][int(len(df)*0.70): int(len(df))])
+print(data_traning.shape)
+print(data_testing.shape)
+
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler(feature_range=(0,1))
+
+data_traning_array = scaler.fit_transform(data_traning)
+
+
+model = load_model('C:/Users/naman/PycharmProjects/Stock_prediction/keras_model.h5')
+# testing
+
+past_100_days = data_traning.tail(100)
+final_df = past_100_days.append(data_testing, ignore_index=True)
+input_data = scaler.fit_transform(final_df)
+
+x_test = []
+y_test = []
+for i in range(100, input_data.shape[0]):
+    x_test.append(input_data[i-100: i])
+    y_test.append(input_data[i, 0])
+
+x_test, y_test = np.array(x_test) , np.array(y_test)
+y_predicted = model.predict(x_test)
+scaler = scaler.scale_
+
+scale_factor = 1/scaler[0]
+y_predicted = y_predicted * scale_factor
+y_test = y_test * scale_factor
+
+
+st.subheader('Prediction vs orignal')
+fig2 = plt.figure(figsize=(12,6))
+plt.plot(y_test,'b',label = 'orignal price')
+plt.plot(y_predicted,'r',label = 'predicted price')
+plt.xlabel('Time')
+plt.ylabel('Price')
+plt.legend()
+st.pyplot(fig2)
